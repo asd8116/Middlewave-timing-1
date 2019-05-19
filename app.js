3d8df28b5648c07cba2c0574f0421708c3e5f75d
wanaka <asd8116@gmail.com>
2019-05-19 11:40:58 +0800

    Add recReqtime middleware

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const app = express()
 const moment = require('moment')
 const port = 3000
 
-const showData = function(req, res, next) {
-  const dayTime = moment().format('YYYY-MM-DD HH:mm:ss')
-  req.showData = dayTime + ' | ' + req.method + ' from ' + req.originalUrl + ' | total time: '
+const recReqTime = function(req, res, next) {
+  req.time = moment()
   next()
 }
 
+const showData = function(req, res, next) {
+  const reqTime = req.time.format('YYYY-MM-DD HH:mm:ss')
+  const resTime = moment()
+  const diff = resTime.diff(req.time)
+
+  req.showData = `${reqTime} | ${req.method} from ${req.path} | total time: ${diff}ms`
+  next()
+}
+
+app.use(recReqTime)
 app.use(showData)
 
 // 列出全部 Todo
